081670aeec4c070f1f6759485f1298edc43ebbea
Jatu <jatu@hotmail.fi>
2026-04-29 03:59:09 +0300

    Delete Javascript/scripts directory

--- Javascript/scripts/helloworld.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    console.log("Tämä kirjoitetaan selaimen konsoliin");
-}
-function sayHelloTo(nimi) {
-alert("Hei " + nimi + "!");
-console.log("sayHello-funktio suoritettu");
-}
